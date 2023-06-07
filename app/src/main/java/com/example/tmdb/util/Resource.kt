91608cb8a51ddd8c888ea2f7a1f428da28284ceb
Ammar <ammaralghifary@gmail.com>
2023-06-07 11:40:00 +0700

    06/07/23
    - Finishing utils

---
+++ app/src/main/java/com/example/tmdb/util/Resource.kt
@@ -0,0 +1,8 @@
+package com.example.tmdb.util
+
+sealed class Resource<out T> {
+
+    data class Success<out T>(val data: T) : Resource<T>()
+
+    data class Error(val message: String) : Resource<Nothing>()
+}
